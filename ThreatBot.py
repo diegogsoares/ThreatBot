8b44b02defef10730231906d578f62a0b0469ff8
Diego Soares <diegogsoares@gmail.com>
2017-03-19 19:03:34 -0400

    1

--- ThreatBot.py
+++ ThreatBot.py
@@ -102,7 +102,7 @@ def CHECK_SPAM_BL (input_value,input):
             except dns.resolver.NXDOMAIN:
                 loop_count += 1
         if loop_count > 0:
-            print_msg = 'IP not listed in ' + loop_count + 'Blacklists'
+            print_msg = 'IP not listed in ' + str(loop_count) + 'Blacklists'
 
     else:
         try:
