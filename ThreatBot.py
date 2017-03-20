8bb63344b47b55b3614e6b256e6c3eca16b659a0
Diego Soares <diegogsoares@gmail.com>
2017-03-20 10:44:02 -0400

    1

--- ThreatBot.py
+++ ThreatBot.py
@@ -620,9 +620,9 @@ def index(request):
             msg_vt = CHECK_HASH_VT(in_message)
             sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg_vt})
         elif validuser == True:
-            msg = "Invalid input!! Use IPs, Domains or Hashes."
-            msg_mark = "**Invalid input!!** \n\n Use IPs, Domains or Hashes."
-            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg, "markdown": msg_mark})
+#            msg = "Invalid input!! Use IPs, Domains or Hashes."
+            msg_mark = "\n ###Invalid input!!** \n Use IPs, Domains or Hashes."
+            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg_mark, "markdown": msg_mark})
         else:
             msg_mark = "**Unauthorized User!!** \n\n Please contact Diego Soares - disoares@cisco.com to request Access\n"
             msg = "Unauthorized User!!  Please contact Diego Soares - disoares@cisco.com to request Access"
