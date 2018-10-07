cc0ae87dac6697859243631c5dfc1158f09d16ee
Diego Soares <diegogsoares@gmail.com>
2018-10-06 21:14:06 -0700

    1

--- TB_TG.py
+++ TB_TG.py
@@ -19,7 +19,7 @@ tg_url = 'https://panacea.threatgrid.com/api/v2/'
 ########## Function CHECK IP ThreatGrid
 ##########
 ######################################################
-def CHECK_QUERY_TG (input_value,input):
+def CHECK_INTEL_TG (input_value,input):
 
     parameters_ip_tg='api_key='+credential.tg_apikey+'&q='+input_value+'&limit=50'
     resp_ip_tg = requests.get(tg_url+'search/submissions?', params=parameters_ip_tg, verify=False)
@@ -29,11 +29,23 @@ def CHECK_QUERY_TG (input_value,input):
         return "ThreatGrig Error: API Call Status " + str(resp_ip_tg.status_code)
 
     resp_ip_tg_json = resp_ip_tg.json()
-
     samples_tg_count = str(resp_ip_tg_json['data']['current_item_count'])
 
+    ################
+    ### ADD TG INFORMATION
+    ################
+    if input == 'ip':
+        print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/#/ips/" + input_value + "\n"
+    elif input == "domain":
+        print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/#/domains/" + input_value + "\n"
+    elif input =='hash':
+        print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/artifacts/" + input_value + "\n"
+
+    ################
+    ### ADD TG SAMPLES
+    ################
     if resp_ip_tg_json['data']['current_item_count'] == 0:
-        print_msg = "NO information was found on " + input_value + "\n"
+        print_msg = "NO Samples were found on " + input_value + "\n"
     else:
         print_msg = "We found " + samples_tg_count + " Malware Samples!\n\tThis is/are some sample(s) found:\n"
 
@@ -43,15 +55,18 @@ def CHECK_QUERY_TG (input_value,input):
         #i_json = i.json()
         if 'analysis' in i['item']:
             if loop_count <= 5:
-                print_msg = print_msg + " * " + str(i['item']['sha1']) + " (" + str(i['item']['analysis']['threat_score']) + ") - https://panacea.threatgrid.com/mask/#/samples/" + str(i['item']['sample']) + "\n"
+                print_msg = print_msg + " * " + str(i['item']['sha256']) + " (" + str(i['item']['analysis']['threat_score']) + ") - https://panacea.threatgrid.com/mask/#/samples/" + str(i['item']['sample']) + "\n"
                 loop_count += 1
 
+    ################
+    ### ADD TG LINK
+    ################
     if input == 'ip':
         print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/#/ips/" + input_value + "\n"
     elif input == "domain":
         print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/#/domains/" + input_value + "\n"
     elif input =='hash':
-        print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/#/search/samples?term=freeform&q=" + input_value + "\n"
+        print_msg = print_msg + " More information @ https://panacea.threatgrid.com/mask/artifacts/" + input_value + "\n"
 
     logger.info("TG OK!")
     print("TG OK!")
@@ -90,8 +105,7 @@ def ACTIVITY_QUERY_TG (input_value,input):
         #i_json = i.json()
         if 'analysis' in i['item']:
             if loop_count <= 5:
-#                print_msg = print_msg + "\t" + str(i.get("item").get("sha1")) + " (" + str(i.get("item").get("analysis").get("threat_score")) + ") - https://panacea.threatgrid.com/mask/#/samples/" + str(i.get("item").get("sample")) + "\n"
-                print_msg = print_msg + "\t" + str(i['item']['sha1']) + " (" + str(i['item']['analysis']['threat_score']) + ") - https://panacea.threatgrid.com/mask/#/samples/" + str(i['item']['sample']) + "\n"
+                print_msg = print_msg + "\t" + str(i['item']['sha256']) + " (" + str(i['item']['analysis']['threat_score']) + ") - https://panacea.threatgrid.com/mask/#/samples/" + str(i['item']['sample']) + "\n"
                 loop_count += 1
 
     if input == 'ip':
@@ -113,16 +127,16 @@ def ACTIVITY_QUERY_TG (input_value,input):
 ########## If you want to test this file uncomment the nex section
 ##########
 ######################################################
-'''
+#'''
 
 if sys.argv[1] == '-ip':
-    msg_to_print = CHECK_QUERY_TG(sys.argv[2],"ip")
+    msg_to_print = CHECK_INTEL_TG(sys.argv[2],"ip")
     print (msg_to_print)
 elif sys.argv[1] == '-hash':
-    msg_to_print = CHECK_QUERY_TG(sys.argv[2],"hash")
+    msg_to_print = CHECK_INTEL_TG(sys.argv[2],"hash")
     print(msg_to_print)
 elif sys.argv[1] == '-domain':
-    msg_to_print = CHECK_QUERY_TG(sys.argv[2],"domain")
+    msg_to_print = CHECK_INTEL_TG(sys.argv[2],"domain")
     print(msg_to_print)
 else:
     print("Invalid Operation!")
