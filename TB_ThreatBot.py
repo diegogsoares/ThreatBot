d2a7396efa01bb9f609bc43cfc125f41816ca626
Diego Soares <diegogsoares@gmail.com>
2019-03-27 14:45:03 -0700

    1

--- TB_ThreatBot.py
+++ TB_ThreatBot.py
@@ -77,6 +77,7 @@ def index(request):
         further actions are taken. i.e.
     """
     webhook = json.loads(request.body)
+    print (webhook)
     print(webhook['data']['id'])
     result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
 
