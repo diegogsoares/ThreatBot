5b3c6f5fbdfd3dbf55e3912d642c968f586878aa
Diego Soares <diegogsoares@gmail.com>
2019-03-27 16:33:41 -0700

    1

--- TB_ThreatBot.py
+++ TB_ThreatBot.py
@@ -257,17 +257,13 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write("ThreatBot")
-        self.send_response(200)
-
+        
     def do_HEAD(self):
         self._set_headers()
 
     def do_POST(self):
         self._set_headers()
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-
-        self.send_response(200)
         self.end_headers()
 
         content = json.loads(self.data_string.decode('utf-8'))
